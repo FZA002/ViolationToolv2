428fe6c8e62206894271f2b0d80ee02f14a8b422
Frederick Almond <falmond@umd.edu>
2022-07-12 10:28:11 -0400

    Added make_option_buttons to nursing_home_gui.py, refactored FormatPage class

--- src/guis/nursing_home_gui.py
+++ src/guis/nursing_home_gui.py
@@ -170,63 +170,16 @@ class FormatPage(tk.Frame):
         self.instructions = ttk.Label(self, text="Choose which data to include", font=("Times", 15))
         self.instructions.grid(column=1, row=2, columnspan=3, pady=10)\
         
-        self.instructions2 = ttk.Label(self, text="Will include dates and tags chosen or defaults if none are selected", font=("Times", 15))
+        self.instructions2 = ttk.Label(self, text="Will include dates and tag filters or defaults if none are selected", font=("Times", 15))
         self.instructions2.grid(column=1, row=3, columnspan=3, pady=10)
-
-        # Holds buttons
-        self.options = {"US Fines":False, "US Violations":False,
-                            "Top fined organizations per state":False, "Most severe organizations per state":False,
-                            "Sum of fines per state per year":False, "Sum of violations per state per year":False,
-                            "Sum of fines per tag per year":False, "Sum of violations per tag per year":False,
-                            "Create sheet with all territories combined":False, "All Violations":False}
 
         # Frame to hold the buttons and list to access them directly
         self.fm = ttk.Labelframe(self, width=50, border=0)
         self.fm.grid(column=2, row=4)
-        self.boxes = []
-        i = 0
+        self.options = {} # bools for options
+        self.option_buttons = {} # Holds option buttons
+        self.make_option_buttons() # Load option buttons
         
-        # Buttons
-        self.boxes.append(tk.Checkbutton(self.fm, width=35, text="US Fines (Total, yearly)", anchor="w", command=lambda:self.add_option("US Fines")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, width=35, text="US Violations (Total, yearly)", anchor="w", command=lambda:self.add_option("US Violations")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Top fined organizations (Total, yearly)", width=35, anchor="w", command=lambda:self.add_option("Top fined organizations per state")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Most severe organizations (Total, yearly)", width=35, anchor="w", command=lambda:self.add_option("Most severe organizations per state")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Sum of fines per state (Total, yearly)", width=35, anchor="w", command=lambda:self.add_option("Sum of fines per state per year")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Sum of violations per state (Total, yearly)", width=35, anchor="w", command=lambda:self.add_option("Sum of violations per state per year")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Sum of fines per tag (Total, yearly)", width=35, anchor="w", command=lambda:self.add_option("Sum of fines per tag per year")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Sum of violations per tag (Total, yearly)", width=35, anchor="w", command=lambda:self.add_option("Sum of violations per tag per year")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Create sheet with all territories combined", width=35, anchor="w", command=lambda:self.add_option("Create sheet with all territories combined")))
-        self.boxes[i].grid()
-        i += 1
-
-        self.boxes.append(tk.Checkbutton(self.fm, text="Create sheet for all violations without territories", width=35, anchor="w", command=lambda:self.add_option("All Violations")))
-        self.boxes[i].grid()
-        i += 1
-
         # Butttons
         self.all_btn = tk.Button(self, command=lambda:self.select_all(), text="Select All", font="Times", bg="#000099", fg="#00ace6", height=1, width=30)
         self.all_btn.grid(column=2, row=5, pady=15)
@@ -236,36 +189,52 @@ class FormatPage(tk.Frame):
         self.fin_btn.grid(column=2, row=6, pady=5)
 
         # Sets all boxes to have no checkmark - need this so a user can click back in 
-        for box in self.boxes:
-            box.deselect()
+        for option in self.option_buttons:
+            self.option_buttons[option].deselect()
 
 
-    # Once user is done selecting options
     def finish(self):
+        ''' Once user is done selecting options. '''
         self.controller.options["Nursing Home"] = self.options
         self.controller.resize_optionspage()
         self.controller.show_frame(OptionsPage)
         print(f"Chosen Nursing Home Options: {self.controller.options['Nursing Home']}")
-        for button in self.boxes:
-            button.destroy()
+        for option in self.option_buttons:
+            self.option_buttons[option].destroy()
         self.fm.destroy()
         FormatPage.destroy(self)
 
-    # Add a chosen option to a list
+
     def add_option(self, opt):
+        ''' Add a chosen option to a list. '''
         self.options[opt] = not self.options[opt]
 
-    # Select all button functionality
+    
     def select_all(self):
+        ''' Select all button functionality. '''
         if self.all:    
             self.options = {k: False for k, _ in self.options.items()}
-            for box in self.boxes:
-                box.deselect()   
+            for option in self.option_buttons:
+                self.option_buttons[option].deselect()   
             self.all = False
             self.all_btn.config(text="Select All")   
         else:         
             self.options = {k: True for k, _ in self.options.items()}
-            for box in self.boxes:
-                box.select()
+            for option in self.option_buttons:
+                self.option_buttons[option].select()
             self.all = True
-            self.all_btn.config(text="Unselect All")   
+            self.all_btn.config(text="Unselect All")
+
+    def make_option_buttons(self):
+        ''' Create buttons that allow the user to exclude include certain extra data. '''
+        options = ["US Fines (Total, yearly)", "US Violations (Total, yearly)", "Top fined organizations (Total, yearly)", 
+                    "Most severe organizations (Total, yearly)", "Sum of fines per state (Total, yearly)",
+                    "Sum of violations per state (Total, yearly)", "Sum of fines per tag (Total, yearly)",
+                    "Sum of violations per tag (Total, yearly)", "Create sheet with all territories combined",
+                    "Create sheet for all violations without territories"]
+        
+        # Make a button for each option and add it to options
+        for option in options:
+            self.options[option] = False
+            self.option_buttons[option] = tk.Checkbutton(self.fm, width=35, text=option, anchor="w", command=lambda x=option: self.add_option(x))
+            self.option_buttons[option].grid()
