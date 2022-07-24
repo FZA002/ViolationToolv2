3f8cdd7c2e997e125688cee4dde55f0069a48ddd
Frederick Almond <falmond@umd.edu>
2022-07-23 21:11:15 -0400

    Removed dataframes from build_exe

--- src/build_exe.sh
+++ src/build_exe.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 pyinstaller --noconfirm --onefile --console --name "ViolationTool" \
---add-data "/Users/Freddie/Impruvon/ViolationToolv2/src/dataframes:dataframes/" \
 --add-data "/Users/Freddie/Impruvon/ViolationToolv2/src/images:images/" \
 --add-data "/Users/Freddie/Impruvon/ViolationToolv2/src/guis:guis/" \
 --add-data "/Users/Freddie/Impruvon/ViolationToolv2/src/assets:assets" \
